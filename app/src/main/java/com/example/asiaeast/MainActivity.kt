4eb7e6bf03549c7476d6c06757b10e034bb51d91
Adrian Lee <adrianlee0118@gmail.com>
2020-02-18 21:04:21 -0800

    Add view binding

--- app/src/main/java/com/example/asiaeast/MainActivity.kt
+++ app/src/main/java/com/example/asiaeast/MainActivity.kt
@@ -18,24 +18,56 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
+        //The two arrays for the spinner adapters. Cities is var because it depends on countries' selection.
         val countries = resources.getStringArray(R.array.countries)
+        var cities = arrayOf("Please choose a country first")
 
-        // access the spinner
+        //Set the country spinner
         if (binding.countrySpinner != null) {
-            val adapter = ArrayAdapter(this, R.layout.spinner_item, countries)
-            adapter.setDropDownViewResource(R.layout.spinner_item)
-            binding.countrySpinner.adapter = adapter
+            val countryadapter = ArrayAdapter(this, R.layout.spinner_item, countries)
+            countryadapter.setDropDownViewResource(R.layout.spinner_item)
+            binding.countrySpinner.adapter = countryadapter
             binding.countrySpinner.setPrompt("Please Select")
 
 
             binding.countrySpinner.onItemSelectedListener = object :
                 AdapterView.OnItemSelectedListener {
                 override fun onItemSelected(parent: AdapterView<*>, view: View, position: Int, id: Long) {
+                    cities = when (countries[position]) {
+                        "South Korea" -> resources.getStringArray(R.array.korean_cities)
+                        "Japan" -> resources.getStringArray(R.array.japanese_cities)
+                        "Taiwan" -> resources.getStringArray(R.array.taiwanese_cities)
+                        "Thailand" -> resources.getStringArray(R.array.thai_cities)
+                        "Vietnam" -> resources.getStringArray(R.array.vietnamese_cities)
+                        "China" -> resources.getStringArray(R.array.chinese_cities)
+                        else -> {
+                            arrayOf("Please choose a country first")
+                        }
+                    }
                     Toast.makeText(this@MainActivity, getString(R.string.selected_item) + " " + "" + countries[position], Toast.LENGTH_SHORT).show()
                 }
 
                 override fun onNothingSelected(parent: AdapterView<*>) {
                     Toast.makeText(this@MainActivity, "Please select a country", Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+
+        //Set the city spinner
+        if (binding.citySpinner != null) {
+            val cityadapter = ArrayAdapter(this, R.layout.spinner_item, countries)
+            cityadapter.setDropDownViewResource(R.layout.spinner_item)
+            binding.citySpinner.adapter = cityadapter
+            binding.citySpinner.setPrompt("Please Select")
+
+            binding.citySpinner.onItemSelectedListener = object :
+                AdapterView.OnItemSelectedListener {
+                override fun onItemSelected(parent: AdapterView<*>, view: View, position: Int, id: Long) {
+                    Toast.makeText(this@MainActivity, getString(R.string.selected_item) + " " + "" + cities[position], Toast.LENGTH_SHORT).show()
+                }
+
+                override fun onNothingSelected(parent: AdapterView<*>) {
+                    Toast.makeText(this@MainActivity, "Please select a city", Toast.LENGTH_SHORT).show()
                 }
             }
         }
