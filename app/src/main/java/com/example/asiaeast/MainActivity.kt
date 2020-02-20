8a1737395456b7ee1696ae0ff94054cfb04f7c6a
Adrian Lee <adrianlee0118@gmail.com>
2020-02-19 23:25:46 -0800

    Add view binding

--- app/src/main/java/com/example/asiaeast/MainActivity.kt
+++ app/src/main/java/com/example/asiaeast/MainActivity.kt
@@ -91,7 +91,7 @@ class MainActivity : AppCompatActivity() {
         }
 
         binding.GoButton.setOnClickListener {
-            
+
             Toast.makeText(this@MainActivity, "Let's Go.", Toast.LENGTH_SHORT).show()
         }
     }
