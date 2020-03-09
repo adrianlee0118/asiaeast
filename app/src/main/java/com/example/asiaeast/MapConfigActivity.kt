aef4e614e9833c1350d2218197c171f61ceb24b9
Adrian Lee <adrianlee0118@gmail.com>
2020-03-08 20:06:12 -0700

    Added no action bar style for in-navigation menu

--- app/src/main/java/com/example/asiaeast/MapConfigActivity.kt
+++ app/src/main/java/com/example/asiaeast/MapConfigActivity.kt
@@ -9,52 +9,9 @@ import androidx.fragment.app.Fragment
 
 class MapConfigActivity : AppCompatActivity() {
 
-    private var fragmentBonjour: Fragment? = null;
-    private var fragmentHello: Fragment? = null;
-    private var fragmentOla: Fragment? = null;
-
-    private val FRAGMENT_BONJOUR_ID = 0
-    private val FRAGMENT_HELLO_ID = 1
-    private val FRAGMENT_OLA_ID = 2
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        setSupportActionBar(findViewById(R.id.toolbar))
-
-        navigation_view.setNavigationItemSelectedListener{
-            when (it.itemId) {
-                R.id.nav_home -> {
-                    true
-                }
-            }
-            true
-        }
-
-        val drawerToggle = ActionBarDrawerToggle(this, drawer, R.string.open, R.string.close)
-        drawer.addDrawerListener(drawerToggle)
-        drawerToggle.syncState()
-
-        supportActionBar?.setDisplayHomeAsUpEnabled(true)
     }
 
-    override fun onBackPressed() {
-        if (drawer.isDrawerOpen(GravityCompat.START)) {
-            drawer.closeDrawer(GravityCompat.START)
-        } else {
-            super.onBackPressed()
-        }
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        super.onOptionsItemSelected(item)
-        return when (item.itemId) {
-            android.R.id.home -> {
-                drawer.openDrawer(GravityCompat.START)
-                true
-            }
-            else -> super.onOptionsItemSelected(item)
-        }
-    }
 }
