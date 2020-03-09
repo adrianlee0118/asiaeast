2b8504961add0b665ec66e562e139b318c393379
Adrian Lee <adrianlee0118@gmail.com>
2020-03-09 13:47:17 -0700

    Added dependency google.materials to allow navigation view usage compile, added view binding in map_config activity

--- app/src/main/java/com/example/asiaeast/MapConfigActivity.kt
+++ app/src/main/java/com/example/asiaeast/MapConfigActivity.kt
@@ -1,17 +1,20 @@
 package com.example.asiaeast
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.MenuItem
-import androidx.appcompat.app.ActionBarDrawerToggle
-import androidx.core.view.GravityCompat
-import androidx.fragment.app.Fragment
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import com.example.asiaeast.databinding.MapConfigBinding
+
 
 class MapConfigActivity : AppCompatActivity() {
 
+    private lateinit var binding: MapConfigBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.map_config)
+        setSupportActionBar(binding.toolbar)
+        
     }
 
 }
