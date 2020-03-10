528a2a4f6fb534ff7d6232a516131181bfc781cf
Adrian Lee <adrianlee0118@gmail.com>
2020-03-09 20:21:52 -0700

    Added key dependencies for navigation drawer, started building new mainactivity class.

--- app/src/main/java/com/example/asiaeast/MapConfigActivity.kt
+++ app/src/main/java/com/example/asiaeast/MapConfigActivity.kt
@@ -1,23 +1,28 @@
 package com.example.asiaeast
 
 import android.os.Bundle
+import android.view.MenuItem
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.GravityCompat
 import androidx.databinding.DataBindingUtil
 import com.example.asiaeast.databinding.MapConfigBinding
 import com.example.asiaeast.fragments.*
+import com.google.android.material.navigation.NavigationView
 import kotlinx.android.synthetic.main.map_config.*
+import kotlinx.android.synthetic.main.map_config.view.*
 
 
-class MapConfigActivity : AppCompatActivity() {
+class MapConfigActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
 
     private lateinit var binding: MapConfigBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.map_config)
-        setSupportActionBar(binding.toolbar)
+        setSupportActionBar(binding.root.toolbar)
+
+        binding.root.nav_view.setNavigationItemSelectedListener { this }
 
         val toggle = ActionBarDrawerToggle(
             this,
@@ -27,8 +32,36 @@ class MapConfigActivity : AppCompatActivity() {
             R.string.navigation_drawer_close
         )
 
-        drawer_layout.addDrawerListener(toggle)
+        binding.root.drawer_layout.addDrawerListener(toggle)
         toggle.syncState()
+
+        if (savedInstanceState == null) {
+            supportFragmentManager().begin().replace(R.id.fragment_container, MapFragment())
+                .commit()
+            binding.root.nav_view.setCheckedItem(R.id.navItemMap)
+        }
+    }
+
+    override fun onNavigationItemSelected(p0: MenuItem): Boolean {
+        when (p0.itemId) {
+            R.id.navItemEditPreferences -> supportFragmentManager().begin().replace(
+                R.id.fragment_container,
+                EditInputsFragment()
+            ).commit()
+            R.id.navItemMap -> supportFragmentManager().begin().replace(
+                R.id.fragment_container,
+                MapFragment()
+            ).commit()
+            R.id.navItemSchedPreview -> supportFragmentManager().begin().replace(
+                R.id.fragment_container,
+                SchedulePreviewFragment()
+            ).commit()
+            R.id.mapItemSendToEmail ->
+        }
+
+        binding.root.drawer_layout.closeDrawer(GravityCompat.START)
+
+        return true
     }
 
     override fun onBackPressed() {
