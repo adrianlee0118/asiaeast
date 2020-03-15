ff038b28933ebe1d1bff541dde651de58cc6ba27
Adrian Lee <adrianlee0118@gmail.com>
2020-03-14 19:24:26 -0700

    Added all Firebase dependencies, started adding Firebase into Main activity's viewmodel and created a data class for holding records from Firebase.
    TODO:
    -Manually populate database on Firebase console
    -Add email sign-in authorization in Android app
    -Add livedata updates asynchronously through Kotlin coroutines in the ViewModel
    -Create key fragment logic for maps and schedule UI(integrate with Google Maps)
    -Integrate with Firebase (ensure asyncrhonous data loading through viewmodel) and build travel locations database with Lat and Lng, location minimum

--- app/src/main/java/com/example/asiaeast/models/MainViewModel.kt
+++ app/src/main/java/com/example/asiaeast/models/MainViewModel.kt
@@ -1,42 +1,67 @@
 package com.example.asiaeast.models
 
+import android.content.ContentValues.TAG
+import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.google.firebase.firestore.CollectionReference
+import com.google.firebase.firestore.EventListener
 import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.QuerySnapshot
 
 class MainViewModel : ViewModel() {
 
     private val firestoreDB = FirebaseFirestore.getInstance()       //access to Firebase cloud DB
     private val destinationRef = firestoreDB.collection("destinations")
 
-    private val country = MutableLiveData<String>("")
-    private val city = MutableLiveData<String>("")
-    private val days = MutableLiveData<Int>(-1)
-    private lateinit var locations: MutableLiveData<List<String>>           //to be provided by database
+    private var country = ""                                                        //default values
+    private var city = ""
+    private var days = -1
+    private lateinit var destinations: MutableLiveData<List<Destination>>           //to be provided by database
 
-    fun getCountry(): LiveData<String> {
+    fun getCountry(): String {
         return country
     }
 
     fun setCountry(ncountry: String) {
-        country.value = ncountry
+        country = ncountry
+        //change filter on dataset
     }
 
-    fun getCity(): LiveData<String> {
+    fun getCity(): String {
         return city
     }
 
     fun setCity(ncity: String) {
-        city.value = ncity
+        city = ncity
+        //change filter on city
     }
 
-    fun getDays(): LiveData<Int> {
+    fun getDays(): Int {
         return days
     }
 
     fun setDays(ndays: Int) {
-        days.value = ndays
+        days = ndays
+    }
+
+    fun getDestinations(): LiveData<List<Destination>>{                                 //listener to update destination variable when db is changed
+        destinationRef.addSnapshotListener(EventListener<QuerySnapshot> { value, e ->
+            if (e != null) {
+                Log.w(TAG, "Listen failed.", e)
+                destinations.value = null
+                return@EventListener
+            }
+
+            var savedDestinations : MutableList<Destination> = mutableListOf()
+            for (doc in value!!) {
+                var DestinationItem = doc.toObject(Destination::class.java)
+                savedDestinations.add(DestinationItem)
+            }
+            destinations.value = savedDestinations
+        })
+
+        return destinations
     }
 }
