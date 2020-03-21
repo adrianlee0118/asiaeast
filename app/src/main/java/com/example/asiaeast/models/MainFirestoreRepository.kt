5bd4c6f308a9a7e2a640c96d99455c083c37316a
Adrian Lee <adrianlee0118@gmail.com>
2020-03-21 00:07:15 -0700

    Cleaned up firebase logic. About to integrate coroutine logic.
    TODO:
    -Manually populate database on Firebase console
    -Add email sign-in authorization in Android app
    -Integrate livedata observers and database fetching with asynchronous coroutines
    -Create key fragment logic for maps and schedule UI(integrate with Google Maps)

--- app/src/main/java/com/example/asiaeast/models/MainFirestoreRepository.kt
+++ app/src/main/java/com/example/asiaeast/models/MainFirestoreRepository.kt
@@ -1,20 +1,18 @@
 package com.example.asiaeast.models
 
-import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.CollectionReference
 import com.google.firebase.firestore.FirebaseFirestore
 
 class MainFirestoreRepository {
 
     private var firestoreDB = FirebaseFirestore.getInstance() //access to Firebase cloud DB
-    var user = FirebaseAuth.getInstance().currentUser!!
 
-    //suspend this function since it's a database operation, takes a long time
+    //Todo: Add authorization
+
     //Get collection for mainViewModel to access Firestore DB, mainViewModel does the querying based on its variables
     fun getDestinations(): CollectionReference {
         var collectionReference = firestoreDB.collection("destinations")
         return collectionReference
     }
 
-    //TODO: Add authentication
 }
