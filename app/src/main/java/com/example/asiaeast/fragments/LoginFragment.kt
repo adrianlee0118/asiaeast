704d1f94ff0f0c0944876c2a9dffc7c8f8f87e31
Adrian Lee <adrianlee0118@gmail.com>
2020-04-03 00:35:33 -0700

    Added onclick listeners to login UI and some logic.
    TODO:
    -Manually populate database on Firebase console
    -Add email sign-in authorization in Android app
    -Integrate livedata observers and database fetching with asynchronous coroutines
    -Create key fragment logic for maps and schedule UI(integrate with Google Maps)

--- app/src/main/java/com/example/asiaeast/fragments/LoginFragment.kt
+++ app/src/main/java/com/example/asiaeast/fragments/LoginFragment.kt
@@ -1,10 +1,10 @@
 package com.example.asiaeast.fragments
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
@@ -19,6 +19,7 @@ import kotlinx.android.synthetic.main.fragment_login.*
 
 class LoginFragment : Fragment() {
 
+    private val TAG = "LOGIN FRAGMENT"
     private lateinit var navController: NavController
     private val mainViewModel: MainViewModel by activityViewModels()   //declaration allows access to root activity viewmodel
     private lateinit var auth: FirebaseAuth
@@ -29,23 +30,25 @@ class LoginFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_login, container, false)
-
-        auth = FirebaseAuth.getInstance()
-
-        //TODO: Logic for signing in with email/password as per https://firebase.google.com/docs/auth/android/password-auth
-        //TODO: Update navigation graph and navigation menu, main activity controller visibility for fragments
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         navController = findNavController() //Initialising navController
+        auth = FirebaseAuth.getInstance()
+
+        login_btn.setOnClickListener { view ->
+            signIn(email.text.toString(), password.text.toString())
+        }
+
+        //TODO: Logic for signing in with email/password as per https://firebase.google.com/docs/auth/android/password-auth
+        //TODO: Update navigation graph and navigation menu, main activity controller visibility for fragments
     }
 
-    override fun onStart() {
+    override fun onStart() { //happens after oncreate, go to inputs fragment if authorization is already granted
         super.onStart()
         val currentUser = auth.currentUser
-        //Go to next fragment if ccurrentUser is not null, else don't as per private fun updateUI(user: FirebaseUser?) on the Github page
         goToInputs(currentUser)
     }
 
@@ -58,11 +61,7 @@ class LoginFragment : Fragment() {
             ).show()
             dialogue.text = "Login successful."
 
-            if (user.isEmailVerified) {
-                verifyEmailButton.visibility = View.GONE
-            } else {
-                verifyEmailButton.visibility = View.VISIBLE
-            }
+            //TODO: Intent or companion object action that directs NavController to go to EditInputsFragment
         } else {
             Toast.makeText(
                 getActivity()!!.getBaseContext(),
@@ -73,10 +72,40 @@ class LoginFragment : Fragment() {
         }
     }
 
-    override fun onClick(v: View) {
-        val i = v.id
-        when (i) {
-            login_button -> signIn(email.text.toString(), password.text.toString())
+    private fun signIn(email: String, password: String) {
+        Log.d(TAG, "signIn:$email")
+        if (!validateForm()) {
+            return
         }
+
+        // [START sign_in_with_email]
+        auth.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    Log.d(TAG, "signInWithEmail:success")
+                    val user = auth.currentUser
+                    updateUI(user)
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Log.w(TAG, "signInWithEmail:failure", task.exception)
+                    Toast.makeText(baseContext, "Authentication failed.",
+                        Toast.LENGTH_SHORT).show()
+                    updateUI(null)
+                    // [START_EXCLUDE]
+                    checkForMultiFactorFailure(task.exception!!)
+                    // [END_EXCLUDE]
+                }
+
+                if (!task.isSuccessful) {
+                    status.setText(R.string.auth_failed)
+                }
+            }
+        // [END sign_in_with_email]
+    }
+
+    private fun signOut() {
+        auth.signOut()
+        //TODO: Make a signout button that becomes visible when sign in happens, whereupon the sign in buttons become invisible, this function will make the sign in buttons appear visible again and auth becomes null
     }
 }
